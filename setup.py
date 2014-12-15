2991f5c9c459056cdcb40c969f42d7d5b2fb3bb6
Neil Webber <nw@neilwebber.com>
2014-12-15 14:14:36 -0600

    pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='numerous',
+      version='1.0',
+      description='Python class for NumerousApp API',
+      author='Neil Webber',
+      author_email='nw@neilwebber.com',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+      ],
+      license='BSD',
+      url='https://github.com/outofmbufs/Nappy',
+      py_modules=['numerous'])
+
