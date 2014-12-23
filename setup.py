b9f8dc21f6e9f4af10ac96e66d0efdd23532df4a
Neil Webber <nw@neilwebber.com>
2014-12-23 12:02:22 -0600

    1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name='numerous',
-      version='1.0',
+      version='1.1',
       description='Python class for NumerousApp API',
       author='Neil Webber',
       author_email='nw@neilwebber.com',
