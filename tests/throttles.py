232b043016dd18e1d0f7508f3c0eab66f705e157
Neil Webber <nw@neilwebber.com>
2015-01-23 13:30:40 -0600

    wrap example

--- tests/throttles.py
+++ tests/throttles.py
@@ -3,20 +3,21 @@
 #
 # This throttle policy only allows N requests per M seconds.
 # It is meant as a demonstration so it seems needlessly complicated.
-# What this does is keep a rolling list of timestamps in the M second window.
-# We also implement a policy that says if you are 3/4 of the way to the throttle
-# policy limit then we slow you down to one request per second, in an attempt to
-# avoid you experiencing an even longer delay to roll off a whole bunch of closely
-# spaced requests.
+# It keeps a rolling list of timestamps in the M second window.
+# We also implement a policy that says if you are 3/4 of the way to 
+# the throttle policy limit then we slow you down to one request per 
+# second, in an attempt to avoid you experiencing an even longer delay 
+# to roll off a whole bunch of closely spaced requests.
 #
-# There's no belief here that this is a useful throttle policy, it's just a demo
+# There's no belief here that this is a useful throttle policy; just a demo
 #
 
 import time
 
 #
 # Set this up as:
-#    nr = Numerous(throttle=NperM, throttleData={'stamps' : [], 'N' : 30, 'M' : 20})
+#    params = {'stamps' : [], 'N' : 30, 'M' : 20}
+#    nr = Numerous(throttle=NperM, throttleData=params)
 #
 # which, for example, would limit you to 30 requests per 20 seconds.
 #
@@ -45,10 +46,43 @@ def NperM(nr, tparams, td, up):
         time.sleep(1)
 
     stamps.append(now)
-    td['stamps'] = stamps          # save the possibly-different stamps object back
+    td['stamps'] = stamps    # save the possibly-different stamps object back
 
     # we still have to compute the throttle retry, for which we use the
     # built-in support function
     return up[0](nr, tparams, up[1], up[2])
+
+
+
+
+
+
+#
+# This throttle policy is an amusing hack that allows
+# you to have access to the rate limit variables yourself.
+# (and it simply invokes the default policy for actual throttling)
+#
+
+def throttleWrap(nr, tparams, td, up):
+    td['rate-remaining'] = tparams['rate-remaining']
+    td['rate-reset'] = tparams['rate-reset']
+    return up[0](nr, tparams, up[1], up[2])
+
+
+#
+# To use it, do this:
+#
+#    savedTd = {}
+#    nr = Numerous(throttle=throttleWrap, throttleData=savedTd)
+#
+# Then after any Numerous operation you will have updated savedTd values:
+#
+#    nr.ping()
+#    print(savedTd['rate-remaining'])
+#
+
+
+
+
 
 
