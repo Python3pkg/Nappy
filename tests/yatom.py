4d2b2acf12bebf39a711b771f49ab2f257fbc86a
Neil Webber <nw@neilwebber.com>
2015-05-07 20:33:49 -0500

    Fix up yatom.py atomicity tests

--- tests/yatom.py
+++ tests/yatom.py
@@ -13,7 +13,7 @@ import threading
 #    -c credspec  : the usual
 #    -n num       : total number of requests to send to server
 #    -m metric    : use metric as the test metric (see discussion below)
-#    -p parallel  : number of threads to run in parallel (default is "-n" value)
+#    -p parallel  : number of threads run in parallel (default is 10)
 #    -D           : debug flag
 #    -q           : quiet - don't display all the individual results
 #
@@ -35,35 +35,41 @@ parser.add_argument('-c', '--credspec')
 parser.add_argument('-n', '--nwrites', type=int, default=50)
 parser.add_argument('-D', '--debug', action="count")
 parser.add_argument('-m', '--metric')
-parser.add_argument('-p', '--parallel', type=int, default=-1)
+parser.add_argument('-p', '--parallel', type=int, default=10)
 parser.add_argument('-q', '--quiet', action="store_true")
 parser.add_argument('testtype', choices=['ADD', 'ONLY'])
 args = parser.parse_args()
 
+# sanity on parallel threads ... can't be less than 1
 if args.parallel < 1:
+    args.parallel = args.nwrites
+
+# can't usefully be more than nwrites
+if args.parallel > args.nwrites:
     args.parallel = args.nwrites
 
 #
 # This is the test function that is run in an individual thread, potentially
 # in parallel (multiple threads running this function).
 #
-# Note in particular that we instantiate a local Numerous(), so it will be per-thread
-# (versus the toplevel global one used to set up the test conditions).
-# Similarly we instantiate a local metric (from the test metric's ID), again
-# so it will be local to this thread vs shared across all the threads.
+# Note in particular that we instantiate a local Numerous(), so it will
+# be per-thread (versus the toplevel global one used to set up the test
+# conditions). Similarly we instantiate a local metric (from the
+# test metric's ID), again so it will be local to this thread vs shared
+# across all the threads.
 #
 # The arguments are:
 #    k      - the Numerous API key
 #    mID    - the ID of the metric we should be banging on
 #    n      - the number of calls to make
 #    wargs  - arguments to pass to the metric.write() (aside from newval)
-#    q      - place (queue) for output results that will be picked up in main thread
+#    q      - queue for output results that will be picked up in main thread
 #
 def tester(k, mID, n, wargs, q):
-    # excessively clever lambda that bypasses the voluntary throttle aspect of the
-    # system default policy while still using the 429/500 bit
+    # excessively clever lambda that bypasses the voluntary throttle of
+    # system default policy while still using it for the 429 
     nr = numerous.Numerous(apiKey=k, throttle= lambda nr, tp, td, up:
-                (tp['result-code'] in (429, 500)) and up[0](nr, tp, up[1], up[2]))
+            (tp['result-code'] == 429) and up[0](nr, tp, up[1], up[2]))
 
     testmetric = nr.metric(mID)
     for i in range(n):
@@ -73,16 +79,13 @@ def tester(k, mID, n, wargs, q):
             v = "NoChange"
         q.put(v)
 
-
-
 apiKey = numerous.numerousKey(args.credspec)
 nrMain = numerous.Numerous(apiKey=apiKey)
-
 
 testmetric = None
 
 if args.metric:
-    #  - first attempt to use it as a metric ID. If that works, use that metric.
+    #  - first attempt to use it as a metric ID. If works, use that metric.
     #  - then attempt to look it up ByLabel 'STRING'
     #  - then attempt to look it up ByLabel 'ONE' (regexp processing will work)
     for mt in [ 'ID', 'STRING', 'ONE' ]:
@@ -98,16 +101,17 @@ if args.metric:
 # If we have to create it, use the args.metric as the label, if given.
 deleteIt = False
 if not testmetric:
-    attrs = { 'private' : True,
-              'description' : 'used by yatom testing program. Ok to delete'
-            }
-    testmetric = nrMain.createMetric(args.metric or 'yatom-temp', 0, attrs=attrs)
+    a = { 'private' : True,
+          'description' : 'used by yatom testing program. Ok to delete'
+        }
+    testmetric = nrMain.createMetric(args.metric or 'yatom-temp', 0, attrs=a)
     deleteIt = True
 
 if args.debug:
-    nrMain.debug(10)
-
-
+    if args.debug > 1:
+        nrMain.debug(10)
+    else:
+        nrMain.debug(1)
 
 # always start the testmetric at zero
 testmetric.write(0)
@@ -162,7 +166,17 @@ if not args.quiet:
         except queue.Empty:
             break
 
+exitStatus = 0
+
+if args.testtype == "ADD":
+    if testmetric.read() != args.nwrites:
+        exitStatus = 1
+        if not args.quiet:
+            print("Wrong final value: {}".format(testmetric.read()))
+
 if deleteIt:
     testmetric.crushKillDestroy()
+
+exit(exitStatus)
 
 
